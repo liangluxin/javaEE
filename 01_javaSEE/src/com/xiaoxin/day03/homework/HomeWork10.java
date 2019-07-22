47d6ae076a69f87252d13d888f7af7658abe7fc3
John <liang@qq.com>
2019-07-22 22:07:46 +0800

    03运算符、if语句

---
+++ 01_javaSEE/src/com/xiaoxin/day03/homework/HomeWork10.java
@@ -0,0 +1,20 @@
+package com.xiaoxin.day03.homework;
+
+public class HomeWork10 {
+	public static void main(String[] args) {
+		int x = 2,y=3;
+
+		switch(x)
+		{
+			default:
+				y++; //4
+			case 3:
+				y++; //5
+				break;
+			case 4:
+				y++;
+		}
+
+		System.out.println("y="+y);
+	}
+}
