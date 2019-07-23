3120914c5c3f486d036eff339d891685c60d462f
John <liang@qq.com>
2019-07-23 22:34:37 +0800

    循环语句while修改错误

--- 01_javaSEE/src/com/xiaoxin/day04/循环语句while语句/Demo(1).java
+++ 01_javaSEE/src/com/xiaoxin/day04/循环语句while语句/Demo(1).java
@@ -4,11 +4,9 @@ class Demo {
 	public static void main(String[] args) {
 		for(int x = 1;x<=4;x++){
 		
-			for (int y=1;y<=5 ;y++ ){
 				System.out.print("*"); //不换行
 				System.out.println("*");//换行
 			}
 		
 		}
 	}
-}
